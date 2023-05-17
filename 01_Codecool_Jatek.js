92123df78346c7862fb67dc361c4b9016968dbae
Marcell Farkas <111434425+famazo93@users.noreply.github.com>
2023-05-17 08:33:44 +0000

    counter working, automatic throw, welcome screen

--- 01_Codecool_Jatek.js
+++ 01_Codecool_Jatek.js
@@ -1,6 +1,13 @@
 background = createSprite(200, 200);
 background.setAnimation("stage_1");
 
+endGame  = createSprite(200, 200);
+endGame.setAnimation("space_1");
+endGame.visible = false;
+
+
+
+
 
 //heroes
 var hero1 = createSprite(50, 290);
@@ -20,11 +27,15 @@ boss.setAnimation("boss");
 
 
 //book
-var book = createSprite(250, 200);
+var book = createSprite(200, 90);
 book.scale = 0.2; //height 31.25px
 book.setAnimation("book_1");
 book.visible = true;
 
+// start screen
+welcome = createSprite(200,200);
+welcome.setAnimation("start");
+welcome.scale = 0.15;
 
 //settings
 var speed = 5;
@@ -85,14 +96,78 @@ function fall(hero) {
     fall(hero2);
   };
 //throw
-  if (keyWentDown("space")) {
+
+  function bookThrow() {
     book.x = boss.x;
     book.y = 90;
     book.visible = true;
-    book.velocityY = randomspeed;
+    book.velocityY = randomNumber(1,10);
     book.velocityX = randomNumber(-5,5);
-    } else {
-      book.rotation += 5;
-      };
+  };
+  
+  if (keyWentDown("space")) {
+    welcome.visible = false;
+    bookThrow();
+  }
+  
+  if (book.isTouching(hero1)) {
+    counter1++;
+    book.visible = false;
+    book.velocityY = 0;
+    book.velocityX = 0;
+    book.x = 0;
+    book.y = 0;
+    bookThrow();
+  };
+  
+  if (book.isTouching(hero2)) {
+    counter2++;
+    book.visible = false;
+    book.velocityY = 0;
+    book.velocityX = 0;
+    bookThrow();
+  };
+  
+  if (book.x < 0 || book.x > 400 || book.y > 400) {
+    bookThrow();
+  };
+  
+  if (counter1 >= 10) {
+    book.x = 0;
+    book.y = 0;
+    book.visible = false;
+    hero1.visible = false;
+    hero2.visible = false;
+    boss.visible = false;
+    endGame.visible = true;
+    fill("white");
+    textAlign(CENTER, TOP);
+    textSize(20);
+    text("Well Done!", 200,200);
+    text("Brown is now a Full-Stack Developer!", 200, 220)
+  }
+  
+    if (counter2 >= 10) {
+    book.x = 0;
+    book.y = 0;
+    book.visible = false;
+    hero1.visible = false;
+    hero2.visible = false;
+    boss.visible = false;
+    endGame.visible = true;
+    fill("white");
+    textAlign(CENTER, TOP);
+    textSize(20);
+    text("Well Done!", 200,200);
+    text("Blue is now a Full-Stack Developer!", 200, 220)
+  }
+  
+    
 }
 
+
+
+var date = new Date()
+
+
+console.log(date.getFullYear()+"/"+(date.getMonth()+1)+"/"+date.getDate())
